f7bfc58048efed237ed644bfb1b358e3b54d3783
Doai Tran <josdoaitran@gmail.com>
2023-04-08 11:04:14 +0700

    example_severving_str with python

---
+++ funny_python_code.py
@@ -0,0 +1,8 @@
+#https://www.facebook.com/photo/?fbid=610308917789997&set=pcb.610310191123203
+
+# Serversing a String with Python
+example_str = "josdoaitran"
+reserving_example_str = example_str[::-1]
+print(reserving_example_str)
+# Note: output >>>> nartiaodsoj
+
